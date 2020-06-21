2285b82c12becbfdbefc800c82d13fe3f5bb9b6b
alice maz <alice@alicemaz.com>
2020-06-21 15:10:40 -0500

    nevermind, rename file rather than module

--- lmdb-ht.scm
+++ lmdb-ht.scm
@@ -40,7 +40,7 @@ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 |#
 
-(module lmdb
+(module lmdb-ht
 
         (
          debuglevel
